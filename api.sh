dedf3292bebee796f501f2f79389095f1610fc7f
Alex Konoplyov <84317437+alexkonoplyov@users.noreply.github.com>
2024-08-13 16:10:32 +0300

    Update api.sh

--- api.sh
+++ api.sh
@@ -38,7 +38,7 @@ for i in 0 1 2 3 4
 do
 curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"history.get", "id":1, "auth":"'${apikey}'", "params":{"history":'$i', "hostids":['${param}'], "countOutput":true}}''' "${apiurl}" | cut -d'"' -f 8
 done
-curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"item.get", "id":1, "auth":"'${apikey}'", "params":{"hostids":['${param}'], "output":"itemid"}}''' "${apiurl}" | grep -Eo '[0-9\"]{7,8}' | tr -d '\n' | sed 's/""/","/g' > temp/temp.txt
+curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"item.get", "id":1, "auth":"'${apikey}'", "params":{"hostids":['${param}'], "output":"itemid"}}''' "${apiurl}" | grep -Eo '[0-9\"]{7,8}' | tr -d '\n' | sed 's/""/","/g' > temp/api-temp.txt
 paramitem=$(cat temp/api-temp.txt)
 curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"trend.get", "id":1, "auth":"'${apikey}'", "params":{"itemids":['${paramitem}'], "countOutput":true}}''' "${apiurl}" | cut -d'"' -f 8
 ;;
