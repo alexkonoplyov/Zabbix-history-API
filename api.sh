49738e4da992265073f4a88ae663de86c76a6b47
Alex Konoplyov <84317437+alexkonoplyov@users.noreply.github.com>
2025-02-12 14:46:33 +0300

    Update api.sh

--- api.sh
+++ api.sh
@@ -55,6 +55,16 @@ curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --d
 paramitem=$(cat temp/api-temp.txt)
 curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"trend.get", "id":1, "auth":"'${apikey}'", "params":{"itemids":['${paramitem}'], "countOutput":true}}''' "${apiurl}" | cut -d'"' -f 8
 ;;
+-item) install
+status
+shift
+param=$(jsonparam $@)
+for i in 0 1 2 3 4
+do
+curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"history.get", "id":1, "auth":"'${apikey}'", "params":{"history":'$i', "itemids":['${param}'], "countOutput":true}}''' "${apiurl}" | cut -d'"' -f 8
+done
+curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"trend.get", "id":1, "auth":"'${apikey}'", "params":{"itemids":['${param}'], "countOutput":true}}''' "${apiurl}" | cut -d'"' -f 8
+;;
 -trends) status
 shift
 param=$(jsonparam $@)
