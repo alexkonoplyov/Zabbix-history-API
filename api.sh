fb259b02ce3f898dcbd353fd4d9e68aae62e4a26
Alex Konoplyov <84317437+alexkonoplyov@users.noreply.github.com>
2025-03-07 10:34:11 +0300

    Update api.sh

--- api.sh
+++ api.sh
@@ -51,8 +51,8 @@ for i in 0 1 2 3 4
 do
 curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"history.get", "id":1, "auth":"'${apikey}'", "params":{"history":'$i', "hostids":['${param}'], "countOutput":true}}''' "${apiurl}" | cut -d'"' -f 8
 done
-curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"item.get", "id":1, "auth":"'${apikey}'", "params":{"hostids":['${param}'], "webitems":true, "output":"itemid"}}''' "${apiurl}" | grep -Eo '[0-9\"]{7,9}' | tr -d '\n' | sed 's/""/","/g' > temp/api-temp.txt
-paramitem=$(cat temp/api-temp.txt)
+curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"item.get", "id":1, "auth":"'${apikey}'", "params":{"hostids":['${param}'], "webitems":true, "output":"itemid"}}''' "${apiurl}" | grep -Eo '[0-9\"]{7,9}' | tr -d '\n' | sed 's/""/","/g' > temp/api-tmp.txt
+paramitem=$(cat temp/api-tmp.txt)
 curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"trend.get", "id":1, "auth":"'${apikey}'", "params":{"itemids":['${paramitem}'], "countOutput":true}}''' "${apiurl}" | cut -d'"' -f 8
 ;;
 -item) install
@@ -68,8 +68,8 @@ curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --d
 -trends) status
 shift
 param=$(jsonparam $@)
-curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"item.get", "id":1, "auth":"'${apikey}'", "params":{"hostids":['${param}'], "webitems":true, "output":"itemid"}}''' "${apiurl}" | grep -Eo '[0-9\"]{7,9}' | tr -d '\n' | sed 's/""/","/g' > temp/api-temp.txt
-paramitem=$(cat temp/api-temp.txt)
+curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"item.get", "id":1, "auth":"'${apikey}'", "params":{"hostids":['${param}'], "webitems":true, "output":"itemid"}}''' "${apiurl}" | grep -Eo '[0-9\"]{7,9}' | tr -d '\n' | sed 's/""/","/g' > temp/api-tmp.txt
+paramitem=$(cat temp/api-tmp.txt)
 curl --request POST -s -w "\n" --header "Content-Type: application/json-rpc" --data '''{"jsonrpc":"2.0", "method":"trend.get", "id":1, "auth":"'${apikey}'", "params":{"itemids":['${paramitem}'], "countOutput":true}}''' "${apiurl}" | cut -d'"' -f 8
 ;;
 -login) logout
